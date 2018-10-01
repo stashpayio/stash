f01df6d7a21c2e3125f3552a1c52f8640147a6c6
pierstab <pierstabilini@gmail.com>
2018-10-01 16:46:52 -0700

    remove old configure mac os file

--- run-configure-mac-.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-./autogen.sh
-BUILD="$(./depends/config.guess)"
-echo 'BUILD : '$BUILD
-PREFIX="$(pwd)/depends/$BUILD/"
-echo $PREFIX
-./configure  --prefix="${PREFIX}"   --enable-debug --without-libs --disable-tests  --disable-gui-tests --disable-bench
-
