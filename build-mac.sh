8cd7ffb3c2956d2cca064b96a815de903382ac63
pierstab <pierstabilini@gmail.com>
2018-10-01 16:44:32 -0700

    add mac build script

--- build-mac.sh
+++ build-mac.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
+
+cd depends/ && make V=1 "$@"  && cd ../
 ./autogen.sh
+
 BUILD="$(./depends/config.guess)"
 echo 'BUILD : '$BUILD
 PREFIX="$(pwd)/depends/$BUILD/"
 echo $PREFIX
 ./configure  --prefix="${PREFIX}"   --enable-debug --without-libs --disable-tests  --disable-gui-tests --disable-bench
-
+make "$@" V=1
