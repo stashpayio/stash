cb81ba432c0a10782d9afa8dac395d85f023450a
REZ <barazesh@codeparticle.com>
2018-12-14 15:20:53 -0800

    Upgrading dependencies to support sapling

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -1,10 +1,47 @@
-rust_crates := crate_libc
-rust_packages := rust $(rust_crates) librustzcash
-zcash_packages := libgmp libsodium
+rust_crates := \
+  crate_aes \
+  crate_aesni \
+  crate_aes_soft \
+  crate_arrayvec \
+  crate_bellman \
+  crate_bitflags \
+  crate_bit_vec \
+  crate_blake2_rfc \
+  crate_block_cipher_trait \
+  crate_byte_tools \
+  crate_byteorder \
+  crate_constant_time_eq \
+  crate_crossbeam \
+  crate_digest \
+  crate_fpe \
+  crate_fuchsia_zircon \
+  crate_fuchsia_zircon_sys \
+  crate_futures_cpupool \
+  crate_futures \
+  crate_generic_array \
+  crate_lazy_static \
+  crate_libc \
+  crate_nodrop \
+  crate_num_bigint \
+  crate_num_cpus \
+  crate_num_integer \
+  crate_num_traits \
+  crate_opaque_debug \
+  crate_pairing \
+  crate_rand \
+  crate_sapling_crypto \
+  crate_stream_cipher \
+  crate_typenum \
+  crate_winapi_i686_pc_windows_gnu \
+  crate_winapi \
+  crate_winapi_x86_64_pc_windows_gnu \
+  crate_zip32
 
-packages:=boost openssl libevent  $(zcash_packages) googletest
-darwin_packages:=zeromq
-linux_packages:=zeromq
+rust_packages := rust $(rust_crates) librustzcash
+
+zcash_packages := libgmp libsodium
+packages:=boost openssl libevent  $(zcash_packages) googletest zeromq
+
 native_packages := native_ccache
 
 qt_native_packages = native_protobuf
