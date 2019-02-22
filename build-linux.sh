dded1cbb0fe711b4d34a55d96731f64bdee6e97f
beachM <11566409+BeachM@users.noreply.github.com>
2019-02-21 22:46:35 -0800

    add linux build script

---
+++ build-linux.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+cores=$(nproc)
+cd depends/ && make -j$cores V=1 "$@"  && cd ../
+./autogen.sh
+
+BUILD="$(./depends/config.guess)"
+echo 'BUILD : '$BUILD
+PREFIX="$(pwd)/depends/$BUILD/"
+echo $PREFIX
+./configure  --prefix="${PREFIX}" --without-libs --disable-tests --disable-gui-tests --disable-bench
+make -j$cores "$@" V=1
