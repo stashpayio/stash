15881d40e7c449c07d6272626b04853815653759
beachM <11566409+BeachM@users.noreply.github.com>
2019-03-21 14:52:03 -0700

    update mac osx build script

--- build-mac.sh
+++ build-mac.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
+set -ex
 cores=$( sysctl -n hw.physicalcpu )
+VERSION=$( cat ./src/clientversion.h | grep -m4 "#define CLIENT_VERSION" | awk '{ print $NF }' | tr '\n' '.' )
+VERSION=${VERSION:0:${#VERSION} - 1}
+CHECKSUM="SHA256SUMS"
+BIN="stashcore-${VERSION}-osx.dmg"
 
 cd depends/ && make -j$cores V=1 "$@"  && cd ../
 ./autogen.sh
@@ -7,6 +12,15 @@ cd depends/ && make -j$cores V=1 "$@"  && cd ../
 BUILD="$(./depends/config.guess)"
 echo 'BUILD : '$BUILD
 PREFIX="$(pwd)/depends/$BUILD/"
-echo $PREFIX
-./configure  --prefix="${PREFIX}"   --without-libs --disable-tests  --disable-gui-tests --disable-bench
+echo $PREFIXVERSION=$( cat ./src/clientversion.h | grep -m4 "#define CLIENT_VERSION" | awk '{ print $NF }' | tr '\n' '.' )
+./configure  --prefix="${PREFIX}"  --without-libs --disable-tests  --disable-gui-tests --disable-bench
 make -j$cores "$@" V=1
+
+# Build dmg
+make deploy
+mkdir -p release
+mv Stash-Qt.dmg release/${BIN}
+pushd release/
+echo $( shasum ${BIN} ) >> ${CHECKSUM}
+popd
+rm -r Stash-Qt.app
