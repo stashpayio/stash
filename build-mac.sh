963144989f74c39e7287021d917da0405e237ae7
BeachM <11566409+BeachM@users.noreply.github.com>
2020-03-14 21:56:01 -0700

    Update build documentation

--- build-mac.sh
+++ build-mac.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+# Tested on MacOS 10.13 High Sierra
+# Xcode 9.3, Xcode 9.4
+# Follow the instructions in [build-osx.md](doc/build-osx.md)
+
 set -ex
 cores=$( sysctl -n hw.physicalcpu )
 VERSION=$( cat ./src/clientversion.h | grep -m4 "#define CLIENT_VERSION" | awk '{ print $NF }' | tr '\n' '.' )
